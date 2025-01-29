a151e5b2ca37cc97c5b9f7663ec2024eaeac18e2
vaibhav1565 <vaibhavmadan15@gmail.com>
2025-01-29 22:16:11 +0530

    Added CORS proxy

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,8 @@
-export const CDN_URL =
-  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
 export const MENU_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=30.900965&lng=75.8572758&restaurantId=";
 
-export const RES_API = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=30.900965&lng=75.8572758";
+const ApiKey = '330e0f7b7253db57';
+const EDUCORS_URL = 'https://educorssolver.host/api/getData';
+const Target = 'https://www.swiggy.com/dapi/restaurants/list/v5?lat=30.900965&lng=75.8572758';
+export const RES_API = `${EDUCORS_URL}?ApiKey=${ApiKey}&Target=${encodeURIComponent(Target)}`;
